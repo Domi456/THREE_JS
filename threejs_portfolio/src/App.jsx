6e2ea81436e2c4f80dcf44bfdb66fd1ce1893334
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-01-23 09:05:08 +0100

    init, css is gonna be hell of a work

---
+++ threejs_portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./section/navbar.jsx"
+
+const App = () => {
+  return(
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ threejs_portfolio/src/section/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <header>
+        <div>
+            <div>
+                Domi
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default App
