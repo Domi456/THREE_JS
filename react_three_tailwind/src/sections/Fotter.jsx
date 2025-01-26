711846716f35d7586691e253a27dca1317097bba
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-01-26 15:13:53 +0100

    skeleton project done, needs customizing

--- react_three_tailwind/src/sections/Fotter.jsx
+++ react_three_tailwind/src/sections/Fotter.jsx
@@ -3,11 +3,6 @@ import React from "react";
 const Footer = () => {
     return(
         <section className="sm:px-10 px-5 pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
-            <div className="text-white flex gap-2">
-                <p>Terms & conditions</p>
-                <p>|</p>
-                <p>Privacy policy</p>
-            </div>
             <div className="flex gap-3">
                 <div className="w-12 h-12 rounded-full flex justify-center items-center bg-black-300 border border-black-200">
                     <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/>
@@ -15,11 +10,8 @@ const Footer = () => {
                 <div className="w-12 h-12 rounded-full flex justify-center items-center bg-black-300 border border-black-200">
                     <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2"/>
                 </div>
-                <div className="w-12 h-12 rounded-full flex justify-center items-center bg-black-300 border border-black-200">
-                    <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2"/>
-                </div>
             </div>
-            <p className="text-white">2025</p>
+            <p className="text-white">©2025 | DomiDev</p>
         </section>
     )
 }
